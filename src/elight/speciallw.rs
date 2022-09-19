140fee7cdc8814d45df906ca31b70fc68808a941
ClaudevonRiegan <63413745+ClaudevonRiegan@users.noreply.github.com>
2022-09-19 11:30:19 +0530

    Update speciallw.rs

--- src/elight/speciallw.rs
+++ src/elight/speciallw.rs
@@ -17,9 +17,12 @@ pub unsafe fn special_lw_out(fighter: &mut L2CFighterCommon) -> L2CValue {
     let module_accessor = sv_system::battle_object_module_accessor(lua_state);
     let entry_id = WorkModule::get_int(module_accessor,*FIGHTER_INSTANCE_WORK_ID_INT_ENTRY_ID) as usize;
     let status = LIST.lock().unwrap().list[entry_id].status;
+    let type_atk = LIST.lock.unwrap().list[entry_id].type_atk;
     if status > -1 {
-        let change = Change::new(0,0.0,0.0,-1,false,-1);
-        LIST.lock().unwrap().update_list(change,entry_id);
+        if type_atk == Type::SPECIAL {
+            let change = Change::new(0,0.0,0.0,-1,false,-1,Type::NONE);
+            LIST.lock().unwrap().update_list(change,entry_id);
+        }
         fighter.change_status(L2CValue::I32(status),false.into());
         return L2CValue::I32(1)
     }
